9626c0e206ed5f59cb8fbc71f2cf0b48d211cce5
josa@ltu.se <josa@ltu.se@92f3ddde-c9f4-11de-8bd7-25a952cf5249>
2009-11-05 12:34:14 +0000

    First checkin

---
+++ trunk/mind/importer/Import.java
@@ -0,0 +1,19 @@
+package mind.importer;
+
+import java.io.*;
+public class Import {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void printXMLTag(File file){
+		
+	//	System.out.print(****TAG****);
+	}
+
+}
