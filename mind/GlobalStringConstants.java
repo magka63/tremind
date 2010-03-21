fdc7cc9e2ae58fed5aba10f2fc8865811b1899ce
nawzad.mardan <nawzad.mardan@liu.se>
2010-03-21 23:25:00 +0000

    New changes have been made in StorageDialog.java  and StorageEquation.java in order to fix(Issue nr. 31)

--- mind/GlobalStringConstants.java
+++ mind/GlobalStringConstants.java
@@ -33,7 +33,7 @@ package mind;
 
 public interface GlobalStringConstants {
 
-  final public String reMIND_version = "2.5";
+  final public String reMIND_version = "2.6";
 
   // Strings used in optimization automation
   final public static String OPT_NONE  = "None";
